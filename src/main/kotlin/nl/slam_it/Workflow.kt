c723d9f7cfb74be52d7b64b51dbaa975b6816e79
Sander Lammertink <sander.lammertink@gmail.com>
2022-07-06 20:28:13 +0200

    feat: :sparkles: added double print workflow feature

--- src/main/kotlin/nl/slam_it/Workflow.kt
+++ src/main/kotlin/nl/slam_it/Workflow.kt
@@ -5,4 +5,6 @@ class Workflow {
     fun getWorkflow() = "workflow bug fix"
 
     fun printWorkflow(workflow: String) = println(workflow)
+
+    fun doublePrintWorkflow(workflow: String) = println("$workflow $workflow")
 }
